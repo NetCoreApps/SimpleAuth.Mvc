b1408e717b7aa7a4c015bfb5866a8a3aafba4d8b
Demis Bellot <demis.bellot@gmail.com>
2016-11-03 03:30:24 -0400

    Update deploy scripts

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Set variables
+# set environment variables used in deploy.sh and AWS task-definition.json:
 export IMAGE_NAME=netcoreapps-mvc
 export IMAGE_VERSION=latest
 
 export AWS_DEFAULT_REGION=us-east-1
 export AWS_ECS_CLUSTER_NAME=default
-
 export AWS_VIRTUAL_HOST=mvc.netcore.io
+
+# set any sensitive information in travis-ci encrypted project settings:
+# required: AWS_ACCOUNT_NUMBER, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+# optional: SERVICESTACK_LICENSE
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 source ../deploy-envs.sh
 
-#AWS_ACCOUNT_NUMBER={} set in private variable
 export AWS_ECS_REPO_DOMAIN=$AWS_ACCOUNT_NUMBER.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
 export ECS_SERVICE=$IMAGE_NAME-service
 export ECS_TASK=$IMAGE_NAME-task
@@ -16,7 +15,16 @@ export PATH=$PATH:$HOME/.local/bin # put aws in the path
 envsubst < task-definition.json > new-task-definition.json
 
 eval $(aws ecr get-login --region $AWS_DEFAULT_REGION) #needs AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY envvars
+
+if [ $(aws ecr describe-repositories | jq --arg x $IMAGE_NAME '[.repositories[] | .repositoryName == $x] | any') == "true" ]; then
+    echo "Found ECS Repository $IMAGE_NAME"
+else
+    echo "ECS Repository doesn't exist, Creating $IMAGE_NAME ..."
+    aws ecr create-repository --repository-name $IMAGE_NAME
+fi
+
 docker push $AWS_ECS_REPO_DOMAIN/$IMAGE_NAME:$IMAGE_VERSION
+
 aws ecs register-task-definition --cli-input-json file://new-task-definition.json --region $AWS_DEFAULT_REGION > /dev/null # Create a new task revision
 TASK_REVISION=$(aws ecs describe-task-definition --task-definition $ECS_TASK --region $AWS_DEFAULT_REGION | jq '.taskDefinition.revision') #get latest revision
 SERVICE_ARN="arn:aws:ecs:$AWS_DEFAULT_REGION:$AWS_ACCOUNT_NUMBER:service/$ECS_SERVICE"
