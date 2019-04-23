627f43d787f461252436085a4506c569ee533fba
Demis Bellot <demis.bellot@gmail.com>
2019-04-22 23:28:39 -0400

    Update deploy-envs.sh

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -7,6 +7,8 @@ export IMAGE_VERSION=latest
 export AWS_DEFAULT_REGION=us-east-1
 export AWS_ECS_CLUSTER_NAME=default
 export AWS_VIRTUAL_HOST=mvc.netcore.io
+export LETSENCRYPT_HOST=$AWS_VIRTUAL_HOST
+export LETSENCRYPT_EMAIL=team@servicestack.net
 
 # set any sensitive information in travis-ci encrypted project settings:
 # required: AWS_ACCOUNT_NUMBER, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
